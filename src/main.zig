bc10fcd51674bffd4693d7a86153c1d9b5331a60
aribambang <aribk210@gmail.com>
2024-01-15 03:14:00 +0700

    add more struct function

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,12 @@ const Point = @import("./point.zig").Point;
 
 pub fn main() !void {
     const T = i32;
-    var p = Point(T).makePoint(1, 2);
+    var p1 = Point(T).makePoint(1, 2);
+    var p2 = Point(T).makePoint(2, 1);
 
-    p.printPoint();
+    p1.printPoint();
+    p2.printPoint();
+
+    var p3 = Point(T).plusPoint(p1, p2);
+    p3.printPoint();
 }
--- src/point.zig
+++ src/point.zig
@@ -24,8 +24,24 @@ pub fn Point(comptime T: type) type {
             return p;
         }
 
+        pub fn assignPoint(p: Self) Self {
+            return makePoint(getX(p), getY(p));
+        }
+
         pub fn printPoint(self: Self) void {
             std.debug.print("({},{})\n", .{ getX(self), getY(self) });
+        }
+
+        pub fn plusPoint(p1: Self, p2: Self) Self {
+            return makePoint(getX(p1) + getX(p2), getY(p1) + getY(p2));
+        }
+
+        pub fn pointEqual(p1: Self, p2: Self) bool {
+            return (getX(p1) == getX(p2) and getY(p1) == getY(p2));
+        }
+
+        pub fn isOrigin(p: Self) bool {
+            return (getX(p) == 0 and getY(p) == 0);
         }
 
         pub fn setX(self: *Self, newX: T) void {
@@ -46,10 +62,43 @@ pub fn Point(comptime T: type) type {
     };
 }
 
+test "init" {
+    const T: type = i32;
+    var p = Point(T).init();
+
+    try std.testing.expect(p.X == 0);
+    try std.testing.expect(p.Y == 0);
+}
+
 test "makePoint" {
     const T: type = i32;
     var p = Point(T).makePoint(1, 2);
 
     try std.testing.expect(p.X == 1);
     try std.testing.expect(p.Y == 2);
+}
+
+test "pointEqual" {
+    const T: type = i32;
+    var p1 = Point(T).makePoint(1, 2);
+    var p2 = Point(T).makePoint(1, 2);
+
+    try std.testing.expect(Point(T).pointEqual(p1, p2) == true);
+}
+
+test "plusPoint" {
+    const T: type = i32;
+    var p1 = Point(T).makePoint(1, 2);
+    var p2 = Point(T).makePoint(1, 2);
+    var p3 = Point(T).plusPoint(p1, p2);
+
+    try std.testing.expect(p3.X == 2);
+    try std.testing.expect(p3.Y == 4);
+}
+
+test "isOrigin" {
+    const T: type = i32;
+    var p = Point(T).init();
+
+    try std.testing.expect(Point(T).isOrigin(p) == true);
 }
